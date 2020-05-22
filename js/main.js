609dfbc782633b3963ac35cc907f7b6de6501140
Michelle Ayumi <44377247+miayuyo@users.noreply.github.com>
2020-05-22 17:13:00 -0300

    upload de páginas de cadastro

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-$(document).ready(function(){
-    function updateText(event){
-      var input=$(this);
-      setTimeout(function(){
-        var val=input.val();
-        if(val!="")
-          input.parent().addClass("floating-placeholder-float");
-        else
-          input.parent().removeClass("floating-placeholder-float");
-      },1)
+$('#chk').click(function () {
+    $('#cadastroCPF').prop('disabled', function(i, v) { return !v; });
+});
+
+var $currentlySelected = null;
+var selected = [];
+
+$('#selectable').selectable({
+    start: function(event, ui) {
+        $currentlySelected = $('#selectable .ui-selected');
+    },
+    stop: function(event, ui) {
+        for (var i = 0; i < selected.length; i++) {
+            if ($.inArray(selected[i], $currentlySelected) >= 0) {
+              $(selected[i]).removeClass('ui-selected');
+            }
+        }
+        selected = [];
+    },
+    selecting: function(event, ui) {
+        $currentlySelected.addClass('ui-selected'); // re-apply ui-selected class to currently selected items
+    },
+    selected: function(event, ui) {
+        selected.push(ui.selected); 
     }
-    $(".floating-placeholder input").keydown(updateText);
-    $(".floating-placeholder input").change(updateText);
+});
+
+var bannerHome = new Swiper ('.swiper-container, .swiper-login', {
+    loop: false,
+    autoplayStopOnLast: true,
+    autoplay: {
+        delay: 2500,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    }
 });
